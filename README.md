231d0f6e128300e54ca4d9b60af302ac6d76c9f0
Guilherme Chimello <guilherme.chimello@gmail.com>
2016-11-30 14:53:33 -0200

    Main Files
    Added first program, allocation list and README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Mechatronics
+
+This is a project i've been proposed to write for a mechatronics class, the project proposed the project
+and building of a simple fermentation system for beers, using a PIC series micro controller and sensors
+for triggering positions in the system.
+
+This will be the repository for the source code of the system, to be loaded to the micro controller using
+a custom library(cuscostdio.h) written by a professor in my institution.
+
+The commands added by the library are:
+
+- output_high();
+- output_low();
+- delay_ms();
+- delay_us();
+- C0=>C7 (outputs)
+- D0=>D7 (inputs)
+- RELE1, RELE2 (Relay Interfaces)
+- BUZZER (exactly what it sounds like.)
---
+++ fermentation.c
@@ -0,0 +1,72 @@
+#include <16f877.h>  // Picversion
+#device ADC=10 // Analog/Digital Converter bitdepth
+#define (clock=20000000) // clock setup
+#include <cuscostdio.h>  // stdio
+
+void fillTank(){
+  while(!D2){
+    output_high(C0);
+    delay_ms(100);
+  }
+  output_low(C0);
+}
+
+void heater(){
+  while(!D0){
+    output_high(C3);
+    delay_ms(100);
+  }
+  output_low(C3);
+}
+
+void cooling(){
+  output_high(C2);
+  delay_ms(6000000);
+  output_low(C2);
+}
+
+void lights(int x){
+  int loop = x;
+  while (loop>=0){
+    output_high(C4);
+    output_high(BUZZER);
+    delay_ms(500);
+    output_low(C4);
+    output_low(BUZZER);
+    delay_ms(500);
+    loop--;
+  }
+}
+
+void emptyTank() {
+  if (D3) {
+    while (!D1) {
+      output_high(C1);
+      lights(4); //Alarms
+    }
+    output_low(C1);
+    output_low(C4);
+  }
+}
+
+main()      //Main doesn't need to be an int
+{
+        output_a(0x00); //Send 0 in hex to outputs
+        output_b(0x00);
+        output_c(0x00);
+        output_d(0x00);
+        int loop=1;
+
+        while(loop>0) //Loop
+        {
+          fillTank();
+          delay_ms(1000);
+          heater();
+          delay_ms(1000);
+          cooling();
+          delay_ms(1000);
+          emptyTank();
+          delay_ms(1000);
+          loop--;
+        }
+}
