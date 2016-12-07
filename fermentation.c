bc81b08e1da1e7bc8eab9fbf38eac85cc87fa12c
Guilherme Chimello <guilherme.chimello@gmail.com>
2016-12-07 16:55:24 -0200

    BugFixes
    Rewriting the program

--- fermentation.c
+++ fermentation.c
@@ -1,37 +1,38 @@
 #include <16f877.h>  // Picversion
 #device ADC=10 // Analog/Digital Converter bitdepth
-#define (clock=20000000) // clock setup
+#use  delay (clock=20000000) // clock setup
 #include <cuscostdio.h>  // stdio
 
 void fillTank(){
-  while(!D2){
-    output_high(C0);
+  while(!input(D2)){
+    output_low(C0);
     delay_ms(100);
   }
-  output_low(C0);
+  output_high(C0);
 }
 
 void heater(){
-  while(!D0){
-    output_high(C3);
+  while(!input(D0)){
+    output_low(C3);
     delay_ms(100);
   }
-  output_low(C3);
+  output_high(C3);
 }
 
 void cooling(){
-  output_high(C2);
-  delay_ms(6000000);
   output_low(C2);
+  delay_ms(6000000);
+  output_high(C2);
 }
 
 int lights(int x){
-  int loop = x;
-  while (loop>=0){
-    output_high(C4);
+  int loop;
+  loop = x;
+  while (loop>=1){
+    output_low(C4);
     output_high(BUZZER);
     delay_ms(500);
-    output_low(C4);
+    output_high(C4);
     output_low(BUZZER);
     delay_ms(500);
     loop--;
@@ -39,13 +40,13 @@ int lights(int x){
 }
 
 void emptyTank() {
-  if (D3) {
-    while (!D1) {
-      output_high(C1);
+  if (input(D3)) {
+    while (!input(D1)) {
+      output_low(C1);
       lights(4); //Alarms
     }
-    output_low(C1);
-    output_low(C4);
+    output_high(C1);
+    output_high(C4);
   }
 }
 
